d14c51e624df60245b4f307605e56551ca43ac40
BasdeBruyn <37217983+BasdeBruyn@users.noreply.github.com>
2018-12-01 20:16:36 +0100

    Merge fix

--- Back_End/src/main/java/api/resources/GenericResource.java
+++ Back_End/src/main/java/api/resources/GenericResource.java
@@ -40,13 +40,6 @@ public abstract class GenericResource<T extends DatabaseObject<T>> {
         return dao.getById(id);
     }
 
-    @GET
-    @Timed
-    @Path("/{id}")
-    public T getFromId(@PathParam("id") Integer id){
-        return dao.getById(id);
-    }
-
     @POST
     @Timed
     public Integer save(@Valid T object){
