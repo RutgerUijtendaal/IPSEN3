e044e2f008e0ede1144af709fb9b1d7455202f26
Jordi735 <jordidorren@gmail.com>
2018-12-11 20:29:50 +0100

    more goodness

--- Front_End/src/app/contact/contact.component.ts
+++ Front_End/src/app/contact/contact.component.ts
@@ -7,7 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  showLocation: boolean;
+  showPhone: boolean;
+  showEmail: boolean;
+
+  constructor() {
+    this.showLocation = true;
+    this.showPhone = true;
+    this.showEmail = true;
+  }
+
+  toggleLocation(): void {
+    this.showLocation = !this.showLocation;
+  }
+
+  togglePhone(): void {
+    this.showPhone = !this.showPhone;
+  }
+
+  toggleEmail(): void {
+    this.showEmail = !this.showEmail;
+
+  }
 
   ngOnInit() {
   }
