b7e3f16779c62ac12abccae7b9e3ee07b3e59c01
Jordi735 <jordidorren@gmail.com>
2018-12-12 01:07:24 +0100

    added coloring of the buttons

--- Front_End/src/app/contact/contact.component.ts
+++ Front_End/src/app/contact/contact.component.ts
@@ -7,33 +7,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  LIGHT_COLOR = '#1e4768';
+  DARK_COLOR = '#03142b';
+
+  locationColor: string;
+  phoneColor: string;
+  mailColor: string;
+
   showLocation: boolean;
   showPhone: boolean;
   showEmail: boolean;
 
+
   constructor() {
-    this.showLocation = true;
+    this.mailColor = this.DARK_COLOR;
+    this.showLocation = false;
     this.showPhone = false;
-    this.showEmail = false;
+    this.showEmail = true;
   }
 
   toggleLocation(): void {
     this.showLocation = true;
+    this.locationColor = this.DARK_COLOR;
     this.showPhone = false;
+    this.phoneColor = this.LIGHT_COLOR;
     this.showEmail = false;
+    this.mailColor = this.LIGHT_COLOR;
   }
 
   togglePhone(): void {
     this.showLocation = false;
+    this.locationColor = this.LIGHT_COLOR;
     this.showPhone = true;
+    this.phoneColor = this.DARK_COLOR;
     this.showEmail = false;
+    this.mailColor = this.LIGHT_COLOR;
   }
 
   toggleEmail(): void {
     this.showLocation = false;
+    this.locationColor = this.LIGHT_COLOR;
     this.showPhone = false;
+    this.phoneColor = this.LIGHT_COLOR;
     this.showEmail = true;
-
+    this.mailColor = this.DARK_COLOR;
   }
 
   ngOnInit() {
