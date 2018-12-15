aabc915cf0960ea8874478620950a67334394611
Jordi735 <jordidorren@gmail.com>
2018-12-15 17:31:30 +0100

    prettyfied the couple-in-list component

--- Front_End/src/app/admin-home/couple-view/couple-list/couple-in-list/couple-in-list.component.ts
+++ Front_End/src/app/admin-home/couple-view/couple-list/couple-in-list/couple-in-list.component.ts
@@ -1,5 +1,6 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, Output} from '@angular/core';
 import {ParentModel} from '../../../../models/parent.model';
+import {CoupleModel} from '../../../../models/couple.model';
 
 @Component({
   selector: 'app-couple-in-list',
@@ -8,12 +9,17 @@ import {ParentModel} from '../../../../models/parent.model';
 })
 export class CoupleInListComponent implements OnInit {
 
-  @Input() parent1: ParentModel;
-  @Input() parent2: ParentModel;
+  @Input() couple: CoupleModel;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  deleteRequest() {
+    console.log('coupleId: ' + this.couple.coupleId);
+    console.log('parent1: ' + this.couple.parent1.id);
+    console.log('parent2: ' + this.couple.parent2.id);
   }
 
 }
