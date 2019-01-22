e7c29d716372c0e25cd12b7c06da8b8d896da473
RutgerUijtendaal <Rutger.uijtendaal@gmail.com>
2019-01-22 14:34:24 +0100

    Parent home page statistics

---
+++ Front_End/src/app/modules/parent/parent-me/parent-statistics/parent-statistics.component.ts
@@ -0,0 +1,48 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {ParentService} from "../../parent.service";
+
+@Component({
+  selector: 'app-parent-statistics',
+  templateUrl: './parent-statistics.component.html',
+  styleUrls: ['./parent-statistics.component.scss']
+})
+export class ParentStatisticsComponent implements OnInit {
+
+  data: [number, number] = [0, 0];
+
+  chartType: string = 'pie';
+  chartDatasets: Array<any> = [
+    { data: this.data,
+      label: 'Resultaten' }
+  ];
+  chartLabels: Array<any> = ['Eens', 'Oneens'];
+  chartColors: Array<any> = [
+    {
+      backgroundColor: ['#46BFBD', '#F7464A'],
+      hoverBackgroundColor: ['#5AD3D1', '#FF5A5E'],
+      borderWidth: 2,
+    }
+  ];
+
+  chartOptions: any = {
+    responsive: true
+  };
+
+  constructor(private parentService: ParentService) {
+    this.parentService.coupleResults[0].forEach((item, index) => {
+      let otherAwnserId: number = this.parentService.coupleResults[1][index].answerId;
+      if(item.answerId !== 0 && otherAwnserId !== 0 ) {
+        if (item.answerId === otherAwnserId) {
+          this.data[0] = this.data[0] + 1;
+        } else {
+          this.data[1] = this.data[1] + 1;
+        }
+      }
+    })
+  }
+
+  ngOnInit() {
+
+  }
+
+}
