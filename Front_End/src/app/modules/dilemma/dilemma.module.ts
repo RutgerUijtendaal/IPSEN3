7a99cac32324ec1cd6ed86a32cedbb5901a58840
Danny van Tol <danny.van.tol@outlook.com>
2019-01-23 16:36:01 +0100

    Add view for answering the dilemma

--- Front_End/src/app/modules/dilemma/dilemma.module.ts
+++ Front_End/src/app/modules/dilemma/dilemma.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DilemmaRoutingModule } from './dilemma-routing.module';
+import { DilemmaViewComponent } from './dilemma-view/dilemma-view.component';
+import { HttpClientModule } from '@angular/common/http';
+import { DilemmaAnswerComponent } from './dilemma-view/dilemma-answer/dilemma-answer.component';
+import { ButtonsModule, ModalModule, PopoverModule, TooltipModule, WavesModule } from 'angular-bootstrap-md';
+import { DilemmaLoadingComponent } from './dilemma-view/dilemma-loading/dilemma-loading.component';
+import { AnswerVerifyComponent } from './dilemma-view/answer-verify/answer-verify.component';
 
+// @ts-ignore
 @NgModule({
-  declarations: [],
+  declarations: [DilemmaViewComponent, DilemmaAnswerComponent, DilemmaLoadingComponent, AnswerVerifyComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule,
+    DilemmaRoutingModule,
+    ModalModule, TooltipModule, PopoverModule, ButtonsModule
   ]
 })
-export class DilemmaModule { }
+export class DilemmaModule {
+}
