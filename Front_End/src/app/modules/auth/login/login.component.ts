0cbdcd820d5a351476c5913e7768187ba43e83fa
Stefan <stefandekeijzer@xs4all.nl>
2019-01-29 11:47:06 +0100

    4

--- Front_End/src/app/modules/auth/login/login.component.ts
+++ Front_End/src/app/modules/auth/login/login.component.ts
@@ -23,7 +23,7 @@ export class LoginComponent implements OnInit {
     this.adminForm = new FormGroup({
       'email': new FormControl('', [Validators.required, ValidateEmail]),
       // TODO
-      'password': new FormControl('', [Validators.required, Validators.minLength(2)])
+      'password': new FormControl('', [Validators.required, Validators.minLength(4)])
     });
   }
 
