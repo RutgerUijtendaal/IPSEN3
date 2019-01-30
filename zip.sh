73592bda1d9f6cd5a2479bbf3431710927574415
Jordi735 <jordidorren@gmail.com>
2019-01-30 19:16:38 +0100

    added quick-zip for deploying

---
+++ zip.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sync;
+cd Front_End && ng build --prod && cd ../Back_End && sync && mvn clean install && cd && sync
+zip -r ipsen3_all.zip Front_End/dist Back_End/target/IPSEN3_Back_End-1.0-SNAPSHOT.jar Back_End/config.yml && sync
